18787d77e9d7155a31fdab467ed37f9e2f86a263
Madan_Govindu <gmmohanreddy92@gmail.com>
2017-10-08 21:22:24 -0500

    Add files via upload

---
+++ HelloWorldServlet/src/govindu/helloworldservlet/servlets/HelloWorld.java
@@ -0,0 +1,48 @@
+package govindu.helloworldservlet.servlets;
+
+import java.io.IOException;
+import java.io.*;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+@WebServlet("/hiddenservlets/HelloWorld2.do")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorld() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out=response.getWriter();
+		double sq= Math.sqrt(4.0);
+		
+		out.println("<html><body><h1 align='center'>Hello world Servlet</h1>"
+				+"Th sqrt of 4.o is" + sq + "</body></html>");
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
